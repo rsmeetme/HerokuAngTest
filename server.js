ef81e50fb13719ebef174cf5625a81e9604ee0cc
rsmeetme <rsmeetme@gmail.com>
2021-12-14 21:02:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 8080;
 app.use(express.static("./dist/anugartest"));
 
 app.get("/*", (req, res, next) => {
-    res.sendFile("index.html", { root: "dist/anugartest" });
+    res.sendFile("index.html", { root: "./dist/anugartest" });
 });
 app.listen(port, () => {
     console.log(`App is running on Localhost:${port}`);
